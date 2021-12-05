0c78ac027f0f111845b4f092e5dc5e5d9565a6a2
jotaalvim <joaoafonsoalvim@gmail.com>
2021-12-05 23:52:19 +0000

    a

--- 2021/03/solve.hs
+++ 2021/03/solve.hs
@@ -1,24 +1,23 @@
-bin2Int :: Int ->  String  -> Int
-bin2Int _ ""      = 0
-bin2Int a ('1':t) = 2^a + bin2Int (a+1) t
-bin2Int a (_:t)   =       bin2Int (a+1) t
+import Data.Char 
 
-finalB = bin2Int 0 . reverse 
+bin2Int ::  String  -> Int
+bin2Int = foldl (\acc x -> acc * 2 + digitToInt x) 0
 
-count :: String -> (Int,Int)
-count ""      = (0  ,0)
-count ('0':t) = (1+p,q) where (p,q) = count t
-count ('1':t) = (p,1+q) where (p,q) = count t
+count :: String -> Char
+count =  chose . foldl (count2) (0,0)
+    where 
+        count2 (a,b) '1' = (a,b+1)
+        count2 (a,b) '0' = (a+1,b)
 
 chose :: (Int,Int) -> Char
-chose (a,b) | a < b = '1' | otherwise = '0' 
+chose (a,b) | a <= b = '1' | otherwise = '0' 
 
 rep :: Char -> Char
 rep '0' = '1'
 rep '1' = '0'
 
 gamma :: [String] -> String
-gamma (h:t) = [ chose $ count $ map (!!k) (h:t) | k <- [0..(length h) -1 ]]
+gamma (h:t) = [ count $ map (!!k) (h:t) | k <- [0..(length h) -1 ]]
 
 epsilon :: String -> String 
 epsilon = map rep
@@ -27,13 +26,17 @@ part1 :: [String] -> Int
 part1 i = e*g
     where
         ga = gamma i
-        g = finalB ga 
-        e = finalB $ epsilon ga
+        g = bin2Int ga 
+        e = bin2Int $ epsilon ga
+
+rating :: [String] -> Int -> String
+rating [x] _ = x
+rating s n = rating (filter ( (s!!n) == $ !! n ) s) (n+1)
+    where g = gamma s
 
 
 main :: IO ()
 main = do
     input <- lines <$> readFile "input.txt"
-
     putStrLn $ "Part 1: " ++ show ( part1 input )
     --putStrLn $ "Part 2: " ++ show ( part2 input )
