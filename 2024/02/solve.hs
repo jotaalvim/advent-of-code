23843ea5bcadd6baf3cb6c2a593f80e582882226
jotaalvim <joaoafonsoalvim@gmail.com>
2024-12-03 01:09:41 +0100

    better this way

--- 2024/02/solve.hs
+++ 2024/02/solve.hs
@@ -2,12 +2,6 @@ import Data.List.Split
 import Data.List
 import Cp
 
-removeNth :: Int -> [] a -> [] a
-removeNth = \n -> \list ->
-      case n of 
-          0 -> tail list
-          otherwise -> head list: removeNth (n-1) (tail list)
-
 
 type Report = [ Int ] 
 
@@ -18,20 +12,20 @@ parseInput =  map parseReport
            . filter (not . null) 
            . splitOn "\n"
 
--- remove 2 elements ar a time and if it passes in the traversal
-
-
 traverseR  = all (uncurry safe) . uncurry zip . (id >< tail) . dup
 
 safe n1 n2 = n2 - n1 <= 3 && n2 > n1 
 
 part1 = length . filter safeReport
 
-safeReport r  = traverseR r || traverseR (reverse r) 
+safeReport r  = traverseR r  || traverseR (reverse r) 
 
-safeReport2 r = traverseR r || traverseR (reverse r) || any traverseR newR || any traverseR (map reverse newR)
+safeReport2 r = safeReport r || any traverseR newR || any traverseR (map reverse newR)
     where
         newR = genP r
+
+removeNth 0 (h:t) = t
+removeNth n (h:t) = h: removeNth (n-1) t
 
 genP l = [ removeNth n l | n <- [0.. length l-1]]
 
