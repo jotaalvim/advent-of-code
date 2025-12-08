88c55ac3da4811251ac8c49ef357ee5225f9ae2e
jotaalvim <joaoafonsoalvim@gmail.com>
2025-12-08 13:31:00 +0000

    part2 day 3

--- 2025/03/solve.hs
+++ 2025/03/solve.hs
@@ -1,10 +1,7 @@
 import Data.List.Split
 import Data.List
 
-infixr 8 ...
 infixr 8 ><
-
-(...) = (.).(.)  -- is the same as .:
 (f >< g) (x,y) = (f x,g y)
 dup a = (a,a)
 
@@ -15,6 +12,7 @@ parseInput = filter (not . null) . splitOn "\n"
 
 -------------------------------------------------------------------------------
 
+-- hardcoded solution for 2 digits  used in part1
 biggest :: String -> Int
 biggest = head . flip concatMap "9876543210" . flip elemIndices  
 
@@ -29,14 +27,22 @@ part1 = sum . map read
       . map (\(a,b) -> map (\e -> b !! e) a ) 
       . map ( getBigIndeces >< id ) 
       . map dup
+--part1 = sum . map read . map (generalSolution 2 "")
 
-part2 = id
+generalSolution :: Int -> String -> String -> String
+generalSolution 0 a l = a
+generalSolution n a l = generalSolution (n-1) (a ++ [big] ) (drop (succ ind ) l)
+    where big = maximum $ take (length l-n+1) l
+          (Just ind) = elemIndex big l
+
+part2 = sum . map read . map (generalSolution 12 "")
+ 
 
 -------------------------------------------------------------------------------
 
 main :: IO ()
 main = do
-    input <- parseInput <$> readFile "input.txt"
+    input <- parseInput <$> readFile "inputExample.txt"
     putStrLn $ "Part 1: " ++ show ( part1 input )
-    -- putStrLn $ "Part 2: " ++ show ( part2 input )
+    putStrLn $ "Part 2: " ++ show ( part2 input )
 
